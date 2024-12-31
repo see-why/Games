a9c17a26831ed9fb314f4ca73f339d18ed0d5401
Iyadi Cyril Osita <iyadicy@gmail.com>
2024-12-30 23:30:30 -0500

    Egg timer util class

---
+++ src/eggtimer/TimerUtil.java
@@ -0,0 +1,39 @@
+package eggtimer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JOptionPane;
+
+class Task extends TimerTask {
+	@Override
+	public void run() {
+		System.out.println("Timer running!.");
+	}
+	
+}
+
+class Alert implements Runnable {
+	@Override
+	public void run() {
+		JOptionPane.showMessageDialog(null, "Egg Cooked");
+	}
+	
+}
+
+public class TimerUtil {
+	public static void runEggTimer() {
+		var timer = new Timer();
+		timer.scheduleAtFixedRate(new Task(), 0L, 60000L);
+		var executor = Executors.newScheduledThreadPool(1);
+		executor.scheduleAtFixedRate(new Alert(), 10, 1L, TimeUnit.MINUTES);
+
+
+		executor.schedule(() -> {
+            timer.cancel();
+            executor.shutdown();
+            System.exit(0);
+        }, 11, TimeUnit.MINUTES);
+	}
+}
